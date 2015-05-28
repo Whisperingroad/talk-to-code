bfad0f68df8afe4e785528f75e54469f2c606099
Whisperingroad <sebastian.wong@hotmail.com>
2015-05-28 15:15:49 +0800

    Minor changes for local computer

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,7 +1,8 @@
 package utils;
 
 public class Constants {
-	public static String HOME = "/home/chunmun/workspace/Talk-to-code";
+	//public static String HOME = "/home/chunmun/workspace/Talk-to-code";
+	public static String HOME = "E:/My Documents/GitHub/talk-to-code";
 	public static String LOGFILE = HOME + "/log/log.txt";
 	public static String INPUTFILE = HOME + "/input/function.txt";
 	public static String SUPPORTED_CLAUSES = HOME + "/input/supportedclauses.txt";
