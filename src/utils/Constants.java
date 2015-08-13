198fe162af262f30166a86a834db300b927d5219
Sebastian Wong <sebastian.wong@hotmail.com>
2015-08-13 17:29:09 +0800

    Configured build path for laptop machine

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,11 +1,22 @@
 package utils;
 
 public class Constants {
+	
+	
 	//public static String HOME = "/home/chunmun/workspace/Talk-to-code";
-	public static String HOME = "E:/My Documents/GitHub/talk-to-code";
+	// for desktop
+	//public static String HOME = "E:/My Documents/GitHub/talk-to-code";
+	//public static String LOGFILE = HOME + "/log/log.txt";
+	//public static String INPUTFILE = HOME + "/input/if-else.txt";
+	//public static String SUPPORTED_CLAUSES = HOME + "/input/supportedclauses.txt";
+	//public static String OUTPUTFILE = HOME + "/output/out.txt";
+	//public static String OUTPUTFILE_CASE = HOME + "/output/atd_out.txt";
+	//for laptop
+	public static String HOME = "C:/Users/Sebastian/Documents/GitHub/talk-to-code";
 	public static String LOGFILE = HOME + "/log/log.txt";
 	public static String INPUTFILE = HOME + "/input/if-else.txt";
 	public static String SUPPORTED_CLAUSES = HOME + "/input/supportedclauses.txt";
 	public static String OUTPUTFILE = HOME + "/output/out.txt";
 	public static String OUTPUTFILE_CASE = HOME + "/output/atd_out.txt";
+	
 }
