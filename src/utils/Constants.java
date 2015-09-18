53ac87b465b9581fa819eef8c9f27c6be765e232
Whisperingroad <sebastian.wong@hotmail.com>
2015-09-18 11:46:17 +0800

    Changed file path

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
 	//public static String OUTPUTFILE = HOME + "/output/out.txt";
 	//public static String OUTPUTFILE_CASE = HOME + "/output/atd_out.txt";
 	//for laptop
-	public static String HOME = "C:/Users/Sebastian/Documents/GitHub/talk-to-code";
+	public static String HOME = "/Users/sebastianwong/Documents/Github Repositories/talk-to-code";
 	public static String LOGFILE = HOME + "/log/log.txt";
 	public static String INPUTFILE = HOME + "/input/if-else.txt";
 	public static String SUPPORTED_CLAUSES = HOME + "/input/supportedclauses.txt";
